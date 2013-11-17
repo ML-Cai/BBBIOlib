0131cd429f9cfcdac021ff904a534c9280264c09
VA_Beaglebonebalck <r7418529@gmail.com>
2013-11-17 17:11:58 +0000

    update BBBIO library

--- BBBio_lib/BBBiolib.c
+++ BBBio_lib/BBBiolib.c
@@ -1,8 +1,15 @@
-// BBBiolib.h
+// BBBiolib.c
 // Simple I/O library of Beaglebone balck
+// 
+// this fnnction is under construction , so mix some libio and BBBIO express ,
+// libio support some basic function for Beaglebone black I/O .
+// BBBIO support I/O function using GPIO as unit .
+// all function will integrated as soon as fast .
+//
 // v1 	October 2013 - shabaz (iolib)
 // v2 	October 2013 - shabaz (iolib)
 // V2.1 November 2013 - VagetableAvenger (BBBlib)
+// V2.2 November 2013 - VagetableAvenger (BBBlib) : add GPIO Enable/Disable function
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,38 +21,38 @@
 #include "BBBiolib.h"
 
 // Memory mapping offset if GPIO , means the memory address of GPIOs
-const unsigned int GPIO_AddressOffset[]={BBBIO_GPIO0, BBBIO_GPIO1, BBBIO_GPIO2, BBBIO_GPIO3};
+const unsigned int GPIO_AddressOffset[]={BBBIO_GPIO0_ADDR, BBBIO_GPIO1_ADDR, BBBIO_GPIO2_ADDR, BBBIO_GPIO3_ADDR};
 
 // GPIO Port number set of Beaglebone Black P8  ,
 // -1 as GND or VCC , 0 / 1 / 2 for GPIO 0/1/2
-const char p8_PortSet[]={-1,-1, 1, 1, 1, 1, 2, 2,
-                         2 , 2, 1, 1, 0, 0, 1, 1,
+const char p8_PortSet[]={-1,-1, 1, 1, 1, 1, 2, 2, 					
+                         2 , 2, 1, 1, 0, 0, 1, 1, 
                          0 , 2, 0, 1, 1, 1, 1, 1,
-                         1 , 1, 2, 2, 2, 2, 0, 0,
-                         0 , 2, 0, 2, 2, 2, 2,-1,
+                         1 , 1, 2, 2, 2, 2, 0, 0,  
+                         0 , 2, 0, 2, 2, 2, 2, 2,
                          2 , 2, 2, 2, 2, 2};
 
 //  GPIO Port ID set of Beaglebone Black P8  ,
-// 0 as GND , offset X as GPIO ID m this value must combine with GPIO number
+// 0 as GND , offset X as GPIO ID m this value must combine with GPIO number	 				   
 const unsigned int p8_PortIDSet[]={
-					0		,0		,1<<6	,1<<7	,1<<2	,1<<3	,1<<2	,1<<3,
+					0		,0		,1<<6	,1<<7	,1<<2	,1<<3	,1<<2	,1<<3, 
 					1<<5	,1<<4	,1<<13	,1<<12	,1<<23	,1<<26	,1<<15	,1<<14,
 					1<<27	,1<<1	,1<<22	,1<<31	,1<<30	,1<<5	,1<<4	,1<<1,
 					1<<0	,1<<29	,1<<22	,1<<24	,1<<23	,1<<25	,1<<10	,1<<11,
-					1<<9	,1<<17	,1<<8	,1<<16	,1<<14	,1<<15	,1<<12	,0,
+					1<<9	,1<<17	,1<<8	,1<<16	,1<<14	,1<<15	,1<<12	,1<<13,
 					1<<10	,1<<11	,1<<8	,1<<9	,1<<6	,1<<7};
-
+					
 // GPIO Port number set of Beaglebone Black P9  ,
 // -1 as GND or VCC , 0 / 1 / 2 for GPIO 0/1/2
 const char p9_PortSet[]={-1,-1,-1,-1,-1,-1,-1,-1,
-	                     -1,-1, 0, 1, 0, 1, 1, 1,
-	                      0, 0, 0, 0, 0, 0, 1,-1,
+	                     -1,-1, 0, 1, 0, 1, 1, 1, 
+	                      0, 0, 0, 0, 0, 0, 1,-1, 
 	                      3,-1, 3, 3, 3,-1, 3,-1,
 	                     -1,-1,-1,-1,-1,-1,-1,-1,
 	                      0, 0,-1,-1,-1,-1};
 
 //  GPIO Port ID set of Beaglebone Black P9  ,
-// 0 as GND , offset X as GPIO ID m this value must combine with GPIO number
+// 0 as GND , offset X as GPIO ID m this value must combine with GPIO number							  
 const unsigned int p9_PortIDSet[]={
 					0		,0		,0		,0		,0		,0		,0		,0,
 					0		,0		,1<<30	,1<<28	,1<<31	,1<<18	,1<<16	,1<<19, 
@@ -53,12 +60,13 @@ const unsigned int p9_PortIDSet[]={
 					1<<21	,0		,1<<19	,1<<17	,1<<15	,0		,1<<14	,0,
 					0		,0		,0		,0		,0		,0		,0		,0,
 					1<<20	,1<<7	,0		,0		,0		,0};
-
-// Memory Handle and Control Handle
+	    
+// Memory Handle and Control Handle		
 int memh=0;
 int ctrlh=0;
 volatile unsigned int *gpio_addr[4]={NULL, NULL, NULL, NULL};
 volatile unsigned int *ctrl_addr=NULL;
+volatile unsigned int *cm_per_addr=NULL;
 
 // pointer to const Port set and Port ID set array
 char* PortSet_ptr[2];
@@ -71,13 +79,13 @@ int
 iolib_init(void)
 {
 	int i;
-
+	
 	if (memh)
 	{
 		if (BBBIO_LIB_DBG) printf("iolib_init: memory already mapped?\n");
 		return(-1);
 	}
-
+	
 	PortSet_ptr[0]=(char*)p8_PortSet;
 	PortSet_ptr[1]=(char*)p9_PortSet;
 	PortIDSet_ptr[0]=(unsigned int*)p8_PortIDSet;
@@ -85,6 +93,14 @@ iolib_init(void)
 	
 	// using memory mapped I/O
 	memh=open("/dev/mem", O_RDWR);
+	
+	// mapping Clock Module Peripheral Registers
+	cm_per_addr = mmap(0, BBBIO_CM_PER_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, memh, BBBIO_CM_PER_ADDR);
+    if(cm_per_addr == MAP_FAILED)
+    {
+        if (BBBIO_LIB_DBG) printf("iolib_init: CM_PER mmap failure!\n");
+        return(-1);
+    }
 	
 	// mapping Address of GPIO 0~4
 	for (i=0; i<4; i++)
@@ -96,17 +112,19 @@ iolib_init(void)
 				return(-1);
 		}
 	}
-	
+
 	// mapping Control Module Registers
+	// for pin mux control , not implemented today
 	if (BBBIO_PINMUX_EN)
 	{
-		ctrl_addr = mmap(0, BBBIO_CONTROL_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, ctrlh, BBBIO_CONTROL_MODULE);
+		ctrl_addr = mmap(0, CONTROL_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, ctrlh, CONTROL_MODULE);
 		if(ctrl_addr == MAP_FAILED)
 		{
 				if (BBBIO_LIB_DBG) printf("iolib_init: control module mmap failure!\n");
 			return(-1);
 		}
 	}
+	
 	
 	// set the bit in the OE register in the appropriate region
 	if (BBBIO_LIB_DBG)
@@ -170,7 +188,7 @@ iolib_setdir(char port, char pin, char dir)
 		printf("iolib_setdir: PortSet_ptr P%d.%d , %X\n",port ,pin , PortSet_ptr[port-8][pin-1]);
 	
 	reg=(void*)gpio_addr[PortSet_ptr[port-8][pin-1]] +BBBIO_GPIO_OE;
-	
+
 	if (dir ==BBBIO_DIR_OUT)
 	{
 		*reg &= ~(PortIDSet_ptr[port-8][pin-1]);
@@ -198,12 +216,16 @@ pin_low(char port, char pin)
 inline char
 is_high(char port, char pin)
 {
+//	printf("IN :%X\n",*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAIN)));
+//	printf("OUT :%X\n",*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAOUT)));
 	return ((*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAIN)) & PortIDSet_ptr[port-8][pin-1])!=0);
 }
 //-----------------------------------------------------------------------------------------------
 inline char
 is_low(char port, char pin)
 {
+//	printf("IN :%X\n",*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAIN)));
+//        printf("OUT :%X\n",*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAOUT)));
 	return ((*((unsigned int *)((void *)gpio_addr[PortSet_ptr[port-8][pin-1]]+BBBIO_GPIO_DATAIN)) & PortIDSet_ptr[port-8][pin-1])==0);
 }
 //-----------------------------------------------------------------------------------------------
@@ -226,4 +248,262 @@ iolib_delay_ms(unsigned int msec)
 	return(0);
 }
 
+//=============================================================================================
+// BBB I/O new function 
+// All function below is unstable
+
+//-----------------------------------------------------------------------------------------------
+/*********************************
+ GPIO Set direction
+ *******************************
+ * Set whole GPIO transmission direction
+ *	@param gpio		: GPIO number , BBBIO_GPIO0 / BBBIO_GPIO1 / BBBIO_GPIO2 / BBBIO_GPIO3 .
+ *	@param inset	: Inpute pin set , using BBBIO_GPIO_PIN_? and | operator to express it.
+ *					  The pin which not selected will keeping origin status .
+ *	@param outset	: Output pin set , format as same as inset .
+ * 		
+ *	@return : 1 for success , 0 for failed
+ *
+ *	@example : BBBIO_GPIO_set_dir(BBBIO_GPIO1 ,
+ *								 BBBIO_GPIO_PIN_11 | BBBIO_GPIO_PIN_12 ,
+ *								 BBBIO_GPIO_PIN_15 | BBBIO_GPIO_PIN_16);
+ *								
+ *	Warring : if you nedd this function , please check value you input , or it may effect other chip or device
+ */	
+
+int 
+BBBIO_GPIO_set_dir(unsigned int  gpio, unsigned int inset , unsigned int outset) 
+{
+	int i;
+	int param_error=0;				// parameter error 
+	volatile unsigned int* reg;		// GPIO register
+	
+	// sanity checks
+	if (memh==0)
+		param_error=1;
+		
+	if (gpio <0 || gpio >3)	//GPIO range
+		param_error=1;
+		
+	if (inset==0 && outset==0)				// pin identify error
+		param_error=1;
+		
+	if (param_error)
+	{
+		if (BBBIO_LIB_DBG) printf("BBBIO_GPIO_set_dir: parameter error!\n");
+		return(-1);
+	}
+	
+	reg=(void*)gpio +BBBIO_GPIO_OE;
+	*reg &= ~outset ;
+	*reg |= inset ;
+	
+	return 0;
+}
+//-----------------------------------------------------------------------------------------------
+/*********************************
+ Check GPIO clock status
+ *******************************
+ * Display GPIO status , this function just display the register value bellow:
+ *	CM_PER_GPIO1_CLKCTRL
+ *	CM_PER_GPIO2_CLKCTRL
+ *	CM_PER_GPIO3_CLKCTRL
+ *
+ * Register format in AM335x Technical Reference Manual ,
+ *		CM_PER_GPIO1_CLKCTRL , session 8.1.12.1.29 , page:947
+ *		CM_PER_GPIO2_CLKCTRL , session 8.1.12.1.30 , page:948
+ *		CM_PER_GPIO3_CLKCTRL , session 8.1.12.1.31 , page:949
+*/
+void
+BBBIO_sys_GPIO_CLK_status()
+{
+	volatile unsigned int* reg ;
+	unsigned int reg_value =0;
+	
+	reg =(void*)cm_per_addr + BBBIO_CM_PER_L4LS_CLKSTCTRL;
+	reg_value = *reg ;
+	printf("CM_PER CM_PER_L4LS_CLKSTCTRL : %X\n", reg_value );
+	
+	reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO1_CLKCTRL;
+	reg_value = *reg ;
+	printf("CM_PER CM_PER_GPIO1_CLKCTRL : %X\n", *reg );
+	printf("\t[18]    OPTFCLKEN_GPIO_1_GDBCLK : %X\n", (reg_value >>18) & 0x01 );
+	printf("\t[17-18] IDLEST                  : %X\n", (reg_value >>16) & 0x02 );
+	printf("\t[0-1]   MODULEMODE              : %X\n", reg_value & 0x02 );
+	
+    reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO2_CLKCTRL;
+	reg_value = *reg ;
+	printf("CM_PER CM_PER_GPIO2_CLKCTRL : %X\n", *reg );
+	printf("\t[18]    OPTFCLKEN_GPIO_2_GDBCLK : %X\n", (reg_value >>18) & 0x01 );
+	printf("\t[17-18] IDLEST                  : %X\n", (reg_value >>16) & 0x02 );
+	printf("\t[0-1]   MODULEMODE              : %X\n", reg_value & 0x02 );
+	
+	
+    reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO3_CLKCTRL;
+	reg_value = *reg ;
+	printf("CM_PER CM_PER_GPIO3_CLKCTRL : %X\n", *reg );
+	printf("\t[18]    OPTFCLKEN_GPIO_3_GDBCLK : %X\n", (reg_value >>18) & 0x01 );
+	printf("\t[17-18] IDLEST                  : %X\n", (reg_value >>16) & 0x02 );
+	printf("\t[0-1]   MODULEMODE              : %X\n", reg_value & 0x02 );
+	
+}
+//-----------------------------------------------------------------------------------------------
+/*********************************
+ Enable GPIO (enable clock)
+ *******************************
+ * Enable GPIO module clock
+ *	@param gpio		: GPIO number , BBBIO_GPIO0 / BBBIO_GPIO1 / BBBIO_GPIO2 / BBBIO_GPIO3 .
+ *
+ *	@return : 1 for success , 0 for failed
+ *
+ *	@example : BBBIO_sys_Enable_GPIO(BBBIO_GPIO2);	// enable GPIO2's module clock
+ *
+ *	Warring : please confirm Device which loaded on this GPIO is Disable , or it may cause some bad effect of these device
+ */
+int BBBIO_sys_Enable_GPIO(unsigned int gpio)		// Enable GPIOx's clock
+{
+	int param_error=0;				// parameter error 
+	volatile unsigned int* reg;		// GPIO register
+
+	// sanity checks
+	if (cm_per_addr==0)
+		param_error=1;
+	if (gpio <0 || gpio >3)	//GPIO range
+		param_error=1;
+		
+	if (param_error)
+	{
+		if (BBBIO_LIB_DBG) printf("BBBIO_sys_Enable_GPIO: parameter error!\n");
+		return 0;
+	}
+	
+	switch(gpio)
+	{
+		case BBBIO_GPIO0 :
+		{
+			printf("Sorry .... GPIO0 not support yet ...\n"); // under construction 
+		}
+		break ;
+		case BBBIO_GPIO1 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO1_CLKCTRL;
+			*reg |= 0x2 ;
+		}
+		break ;
+		case BBBIO_GPIO2 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO2_CLKCTRL;
+			*reg |= 0x2 ;
+		}
+		break ;
+		case BBBIO_GPIO3 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO3_CLKCTRL;
+			*reg |= 0x2 ;
+		}
+		break ;
+		default :
+		{
+			return 0 ;
+		}
+		break ;
+	}
+	return 1 ;
+}
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+/*********************************
+ Disable GPIO (Disable clock)
+ *******************************
+ * Disable GPIO module clock
+ *	@param gpio		: GPIO number , BBBIO_GPIO0 / BBBIO_GPIO1 / BBBIO_GPIO2 / BBBIO_GPIO3 .
+ *
+ *	@return : 1 for success , 0 for failed
+ *
+ *	@example : BBBIO_sys_Disable_GPIO(BBBIO_GPIO2);	// Disable GPIO2's module clock
+ *
+ *	Warring : please confirm Device which loaded on this GPIO is Disable , or it may cause some bad effect of these device
+ */
+int BBBIO_sys_Disable_GPIO(unsigned int gpio)		// Disable GPIOx's clock
+{
+	int param_error=0;				// parameter error 
+	volatile unsigned int* reg;		// GPIO register
+
+	// sanity checks
+	if (cm_per_addr==0)
+		param_error=1;
+	if (gpio <0 || gpio >3)	//GPIO range
+		param_error=1;
+		
+	if (param_error)
+	{
+		if (BBBIO_LIB_DBG) printf("BBBIO_sys_Disable_GPIO: parameter error!\n");
+		return 0;
+	}
+	
+	switch(gpio)
+	{
+		case BBBIO_GPIO0 :
+		{
+			printf("Sorry .... GPIO0 not support yet ...\n"); // under construction 
+		}
+		break ;
+		case BBBIO_GPIO1 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO1_CLKCTRL;
+			*reg &= ~0x2 ;
+		}
+		break ;
+		case BBBIO_GPIO2 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO2_CLKCTRL;
+			*reg &= ~0x2 ;
+		}
+		break ;
+		case BBBIO_GPIO3 :
+		{
+			reg =(void*)cm_per_addr + BBBIO_CM_PER_GPIO3_CLKCTRL;
+			*reg &= ~0x2 ;
+		}
+		break ;
+		default :
+		{
+			return 0 ;
+		}
+		break ;
+	}
+	return 1 ;
+}
+//-------------------------------------------------------------------------------------------
+inline int
+BBBIO_sys_delay_ms(unsigned long msec)
+{
+        int ret;
+        struct timespec a;
+
+        a.tv_nsec=(msec)*1000000L;
+        a.tv_sec=0;
+        if ((ret = nanosleep(&a, NULL)) != 0)
+        {
+	    fprintf(stderr, "delay_ms error: %s\n", strerror(errno));
+        }
+        return 0;
+}
+
+
+//-------------------------------------------------------------------------------------------
+inline int
+BBBIO_sys_delay_us(unsigned long msec)
+{
+        int ret;
+        struct timespec a;
+
+        a.tv_nsec=(msec)*1000L;
+        a.tv_sec=0;
+        if ((ret = nanosleep(&a, NULL)) != 0)
+        {
+            fprintf(stderr, "delay_ms error: %s\n", strerror(errno));
+        }
+        return 0;
+}
 
--- BBBio_lib/BBBiolib.h
+++ BBBio_lib/BBBiolib.h
@@ -1,8 +1,15 @@
 // BBBiolib.h
 // Simple I/O library of Beaglebone balck
+// 
+// this fnnction is under construction , so mix some libio and BBBIO express ,
+// libio support some basic function for Beaglebone black I/O .
+// BBBIO support I/O function using GPIO as unit .
+// all function will integrated as soon as fast .
+//
 // v1 	October 2013 - shabaz (iolib)
 // v2 	October 2013 - shabaz (iolib)
-// V2.1 November 2013 - VagetableAvenger (BBBlib) : add some comment and modify function name
+// V2.1 November 2013 - VagetableAvenger (BBBlib)
+// V2.2 November 2013 - VagetableAvenger (BBBlib) : add GPIO Enable/Disable function
 
 #ifndef _BBBIOLIB_H_
 #define _BBBIOLIB_H_
@@ -15,19 +22,31 @@
 // not implemented today
 #define BBBIO_PINMUX_EN 0
 
-#define BBBIO_CONTROL_MODULE 0x44e10000
-#define BBBIO_CONTROL_LEN 0xA00
+#define CONTROL_MODULE 0x44e10000
+#define CONTROL_LEN 0xA00
+
+
+#define BBBIO_CM_PER_ADDR			0x44e00000
+#define BBBIO_CM_PER_LEN			0x4000
+#define BBBIO_CM_PER_L4LS_CLKSTCTRL	0x0
+#define BBBIO_CM_PER_GPIO1_CLKCTRL	0xAC
+#define BBBIO_CM_PER_GPIO2_CLKCTRL	0xB0
+#define BBBIO_CM_PER_GPIO3_CLKCTRL	0xB4
 
 // AM335X GPIO memory mapping address
 /* @Source : AM335x Technical Reference Manual ,page 171~173
  			 Table 2-2. L4_WKUP Peripheral Memory Map (continued)
 			 Table 2-3. L4_PER Peripheral Memory Map (continued)
 */
-#define BBBIO_GPIO0 0x44e07000
-#define BBBIO_GPIO1 0x4804c000
-#define BBBIO_GPIO2 0x481ac000
-#define BBBIO_GPIO3 0x481ae000
-#define BBBIO_GPIOX_LEN 0x1000	// GPIO length , ex: GPIO0 0x44e07000 ~ 0x44e07FFF
+#define BBBIO_GPIO0 		0
+#define BBBIO_GPIO1 		1
+#define BBBIO_GPIO2 		2
+#define BBBIO_GPIO3			3
+#define BBBIO_GPIO0_ADDR	0x44e07000
+#define BBBIO_GPIO1_ADDR 	0x4804c000
+#define BBBIO_GPIO2_ADDR 	0x481AC000
+#define BBBIO_GPIO3_ADDR 	0x481AE000
+#define BBBIO_GPIOX_LEN 	0x1000	// GPIO length , ex: GPIO0 0x44e07000 ~ 0x44e07FFF
 
 //#define GPIO1_SIZE (GPIO1_END_ADDR - GPIO1_START_ADDR)
 // GPIO control register address
@@ -42,6 +61,8 @@
 #define BBBIO_GPIO_DATAOUT 		0x13C	// Data to set on output pins	, Section 25.4.1.18 ,page : 4658
 #define BBBIO_GPIO_CLEARDATAOUT 0x190	// Clear Data Output Register 	, Section 25.4.1.25 ,page : 4665 , 1 is set pin to Low ,  BUT 0 is no effect
 #define BBBIO_GPIO_SETDATAOUT 	0x194	// Set Data Output Register 	, Section 25.4.1.26 ,page : 4666 , 1 is set pin to HIGH ,  BUT 0 is no effect
+
+#define BBBIO_GPIO_CTRL			0x130	// 
 
 // call this first. Returns 0 on success, -1 on failure
 int iolib_init(void);
@@ -62,23 +83,64 @@ inline char is_low(char port, char pin);
 
 //---------------------------------------------------------
 // call this first. Returns 0 on success, -1 on failure
-int BBBIO_init(void);
+int BBBIO_sys_init(void);
 // call this when you are done with I/O. Returns 0 on success, -1 on failure
-int BBBIO_release(void);
+int BBBIO_sys_release(void);
 
-// Set port direction (DIR_IN/DIR_OUT) where port is 8/9 and pin is 1-46
+
+void BBBIO_sys_GPIO_status() ;
+int BBBIO_sys_Enable_GPIO(unsigned int gpio);		// Enable GPIOx's clock
+int BBBIO_sys_Disable_GPIO(unsigned int gpio);		// Disable GPIOx's clock
+
+
+
+// Set port direction (BBBIO_DIR_IN /BBBIO_DIR_OUT) where port is 8/9 and pin is 1-46
 int BBBIO_set_dir(char port, char pin, char dir);
 
 // set and get pin levels
-// (pin unit)
+// (port/pin unit , control single pin at board)
 inline void BBBIO_set_high(char port, char pin);
 inline void BBBIO_set_low(char port, char pin);
 inline char BBBIO_get(char port, char pin);
 
-// (port unit)
-inline void BBBIO_set_high_port(char port, char pin);
-inline void BBBIO_set_low_port(char port, char pin);
-inline int BBBIO_get_port(char port, char pin);			// GPIO data register 32 bit
+// (GPIO unit , control hole register)
+#define BBBIO_GPIO_PIN_0	1<< 0
+#define BBBIO_GPIO_PIN_1	1<< 1
+#define BBBIO_GPIO_PIN_2	1<< 2
+#define BBBIO_GPIO_PIN_3	1<< 3
+#define BBBIO_GPIO_PIN_4	1<< 4
+#define BBBIO_GPIO_PIN_5	1<< 5
+#define BBBIO_GPIO_PIN_6	1<< 6
+#define BBBIO_GPIO_PIN_7	1<< 7
+#define BBBIO_GPIO_PIN_8	1<< 8
+#define BBBIO_GPIO_PIN_9	1<< 9
+#define BBBIO_GPIO_PIN_10	1<< 10
+#define BBBIO_GPIO_PIN_11	1<< 11
+#define BBBIO_GPIO_PIN_12	1<< 12
+#define BBBIO_GPIO_PIN_13	1<< 13
+#define BBBIO_GPIO_PIN_14	1<< 14
+#define BBBIO_GPIO_PIN_15	1<< 15
+#define BBBIO_GPIO_PIN_16	1<< 16
+#define BBBIO_GPIO_PIN_17	1<< 17
+#define BBBIO_GPIO_PIN_18	1<< 18
+#define BBBIO_GPIO_PIN_19	1<< 19
+#define BBBIO_GPIO_PIN_20	1<< 20
+#define BBBIO_GPIO_PIN_21	1<< 21
+#define BBBIO_GPIO_PIN_22	1<< 22
+#define BBBIO_GPIO_PIN_23	1<< 23
+#define BBBIO_GPIO_PIN_24	1<< 24
+#define BBBIO_GPIO_PIN_25	1<< 25
+#define BBBIO_GPIO_PIN_26	1<< 26
+#define BBBIO_GPIO_PIN_27	1<< 27
+#define BBBIO_GPIO_PIN_28	1<< 28
+#define BBBIO_GPIO_PIN_29	1<< 29
+#define BBBIO_GPIO_PIN_30	1<< 30
+#define BBBIO_GPIO_PIN_31	1<< 31
+
+int BBBIO_GPIO_set_dir(unsigned int  gpio, unsigned int inset , unsigned int outset); 
+inline void BBBIO_GPIO_set_high(char gpio, unsigned int pinset);
+inline void BBBIO_GPIO_set_low(char gpio, unsigned int pinset);
+inline int BBBIO_GPIO_get(char gpio, unsigned int pin);			// GPIO data register 32 bit
 
 #endif // _IOLIB_H_
 
