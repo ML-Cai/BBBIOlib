c57db57aa2775cf0f074636494b9f775ba34dad6
VegetableAvenger <r7418529@gmail.com>
2014-08-13 23:10:41 +0000

    add PWM2 demo , 1's lighting

---
+++ Demo/Demo_PWM/PWM2.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../BBBio_lib/BBBiolib.h"
+/* -------------------------------------------------------------- */
+int main(void)
+{
+	iolib_init();
+
+	//10 000 000
+	BBBIO_PWMSS_Setting(BBBIO_PWMSS0 , 1.0f ,50.0 , 50.0);
+	//printf("PWM %d setting\n", BBBIO_PWMSS0);
+	//sleep(5);
+	printf("PWM %d enable\n", BBBIO_PWMSS0);
+	BBBIO_ehrPWM_Enable(BBBIO_PWMSS0);
+	sleep(3);
+	printf("PWM %d disable\n", BBBIO_PWMSS0);
+	BBBIO_ehrPWM_Disable(BBBIO_PWMSS0);
+	iolib_free();
+	return(0);
+}
+
+
